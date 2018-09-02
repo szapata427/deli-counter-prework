9dedf3a9c1aefd4c43b8eb89b65c0bf7f8fcf3f3
Steven Zapata <szapata427@gmail.com>
2018-09-02 19:08:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 def line(name)
   i = 0
   if name.length > i
+  
     name.each_with_index do |person, idx|
       puts "The line is currently: #{idx + 1}. #{person}."
   else
     puts "The line is currently empty." 
   end
+end
 end
 
  
