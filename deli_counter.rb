b2732fa46191fe95215b3c748f84f3910fa70cf0
Steven Zapata <szapata427@gmail.com>
2018-09-02 19:26:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ if name.length == 0
     puts "The line is currently empty."
     
   else
-    
-   name.each_with_index(0) do |person, idx|
+    name.each_with_index(0) do |person, idx|
      array.push("#{idx + 1}. #{person}.")
    end
  end
