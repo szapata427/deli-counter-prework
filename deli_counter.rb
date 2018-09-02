884900c2c31445b65792948481efb6604166c1e8
Steven Zapata <szapata427@gmail.com>
2018-09-02 19:29:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,11 @@
-def line(name)
-array = []
-if name.length == 0
+def line(x)
+  line_array = []
+  if x.length == 0
     puts "The line is currently empty."
-    
   else
-    name.each_with_index(1) do |person, idx|
-     array.push("#{idx}. #{person}.")
-   end
- end
-   
-  puts "The line is currently: #{array.join(" ")}"
-    
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
- 
+end
